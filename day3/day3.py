c27cbbfced505ed080a579886aa62669c8e6dcfd
Dennis Schwartz <schwartz_dennis@web.de>
2021-12-20 02:27:53 +0000

    First go at day3

--- day3/day3.py
+++ day3/day3.py
@@ -1,14 +1,56 @@
+from typing import List
+
+def get_bit_counts(nums: List[str]) -> List[List[int]]:
+    bitcounts = []
+    for i in range(len(nums[0])):
+        bitcounts.append([0, 0])
+    for bin in nums:
+        for j, bit in enumerate(bin):
+            bitcounts[j][int(bit)] += 1
+    return bitcounts
 
 def solution1(cmds):
-    return 0
+    bitcounts = get_bit_counts(cmds)
+    gamma = ''
+    epsilon = ''
+    for b in bitcounts:
+        if b[0] > b[1]:
+            gamma += '0'
+            epsilon += '1'
+        if b[0] < b[1]:
+            gamma += '1'
+            epsilon += '0'
+        assert b[0] != b[1], f'Not most common bit? {bitcounts}'
+    return int(gamma, 2) * int(epsilon, 2)
 
 
 def solution2(cmds):
-    return 0
+    bitcounts = get_bit_counts(cmds)
+    print(bitcounts)
+    ox = cmds
+    co2 = cmds
+    res_ox = None
+    res_co2 = None
+    for bit, counts in enumerate(bitcounts):
+        most_common = '1' if counts[1] >= counts[0] else '0'
+        if not res_ox:
+            ox = [b for b in ox if b[bit] == most_common]
+        if not res_co2:
+            co2 = [b for b in co2 if b[bit] != most_common]
+        if len(ox) == 1:
+            res_ox = ox[0]
+        if len(co2) == 1:
+            res_co2 = co2[0]
+        if res_ox and res_co2:
+            break
+    print(res_ox, res_co2)
+    print(int(res_ox, 2) * int(res_co2, 2), int(res_ox, 2), int(res_co2, 2))
+    return int(res_ox, 2) * int(res_co2, 2)
+
 
 
 if __name__ == '__main__':
-    with open('day3_input.txt', 'r') as f:
+    with open('test.txt', 'r') as f:
         data = [line.strip() for line in f]
 
     res1 = solution1(data)
